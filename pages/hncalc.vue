62d8fe1d71d8844aae933bcf4259037dc6c264de
Kamil Ślęczka <k.sleczkax@gmail.com>
2022-09-01 23:40:18 +0200

    styles

--- pages/hncalc.vue
+++ pages/hncalc.vue
@@ -8,20 +8,21 @@ const hnCalcStore = useHnCalcStore()
     <div>
         <navbar />
         <main class="flex items-center justify-center h-[calc(100vh-61px)]">
-            <section class="w-2/3 h-full">
-                <h1 class="mb-2">H/N calculator</h1>
+            <section class="w-2/3 px-6 h-full">
+                <h1 class="text-3xl font-bold mt-5 mb-1">H/N calculator</h1>
                 <p>Use this utility to create realistic looking hill profiles.</p>
-                <div class="mb-5">
 
-                    <div class="row mb-3">
-                        <div class="col-6">
+                <div class="mt-6">
+                    <h2 class="text-2xl">Recommended values:</h2>
+                    <div class="flex gap-3">
+                        <div class="flex-auto">
                             <base-input disabled :model-value="
                                 Number.parseFloat((hnCalcStore.k / 30 + 6.9).toFixed(2)) <= 10.5
                                     ? (hnCalcStore.k / 30 + 6.9).toFixed(2)
                                     : 10.5
                             " label="Minimum optimal inrun alpha" description="min-alpha" type="float" />
                         </div>
-                        <div class="col-6">
+                        <div class="flex-auto">
                             <base-input disabled :model-value="
                                 Number.parseFloat((hnCalcStore.k / 30 + 7.9).toFixed(2)) <= 11.5
                                     ? (hnCalcStore.k / 30 + 7.9).toFixed(2)
@@ -29,29 +30,26 @@ const hnCalcStore = useHnCalcStore()
                             " label="Maximum optimal inrun alpha" description="max-alpha" type="float" />
                         </div>
                     </div>
-                    <div class="row mb-3">
-                        <div class="col-6">
+                    <div class="flex gap-3">
+                        <div class="flex-auto">
                             <base-input disabled :model-value="(hnCalcStore.k / 800 + 0.4).toFixed(2)"
                                 label="Minimum optimal H/N" description="min-hn" type="float" />
                         </div>
-                        <div class="col-6">
+                        <div class="flex-auto">
                             <base-input disabled :model-value="(hnCalcStore.k / 1000 + 0.48).toFixed(2)"
                                 label="Maximum optimal H/N" description="max-hn" type="float" />
                         </div>
                     </div>
                 </div>
-                <div class="mb-5">
-                    <div class="mb-3">
-                        <base-input v-model="hnCalcStore.h2n" label="Selected H/N value" description="max-hn"
-                            type="number" step="0.01" min="0.51" max="0.61" />
-                    </div>
-                    <div class="row mb-3">
-                        <div class="col-6">
+                <div class="mt-6">
+                    <h2 class="text-2xl">Computed H & N values:</h2>
+                    <div class="flex gap-3">
+                        <div class="flex-auto">
                             <base-input disabled :model-value="
                                 ((hnCalcStore.k * Math.sin(Math.atan(hnCalcStore.h2n))) / 1.005).toFixed(2)
                             " label="Computed H value" description="h-val" type="float" />
                         </div>
-                        <div class="col-6">
+                        <div class="flex-auto">
                             <base-input disabled :model-value="
                                 ((hnCalcStore.k * Math.cos(Math.atan(hnCalcStore.h2n))) / 1.005).toFixed(2)
                             " label="Computed N value" description="n-val" type="float" />
@@ -68,8 +66,11 @@ const hnCalcStore = useHnCalcStore()
                     <hr class="h-1 w-full">
                 </div>
                 <div class="mb-3">
-                    <base-input v-model="hnCalcStore.k" label="Construction point" description="k-point" type="number"
-                        required />
+                    <base-input v-model="hnCalcStore.k" label="K point" description="k-point" type="number" required />
+                </div>
+                <div class="mb-3">
+                    <base-input v-model="hnCalcStore.h2n" label="Select H/N value between 0,51 and 0,61"
+                        description="max-hn" type="number" step="0.01" min="0.51" max="0.61" required />
                 </div>
             </aside>
         </main>
