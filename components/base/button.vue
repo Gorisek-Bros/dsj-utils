8771ad1e018a2d3b908a5615fcc46a54bfaf6dbb
Grzegorz02@ <grzegorz.perun@gmail.com>
2022-09-11 08:11:55 +0200

    feat: tweak base button styling

--- components/base/button.vue
+++ components/base/button.vue
@@ -1,14 +1,36 @@
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
   disabled?: boolean
   type?: 'button' | 'submit' | 'reset'
   loading?: boolean
+  secondary?: boolean
 }>()
+
+const button = ref<HTMLButtonElement>(null)
+const initialHeight = ref(0)
+
+onMounted(() => initialHeight.value = button.value.clientHeight)
+watch(() => props.loading, () => {
+  if (props.loading)
+    button.value.style.height = `${initialHeight.value}px`
+  else
+    button.value.style.height = 'auto'
+})
 </script>
 
 <template>
-  <button class="flex justify-center text-blue-600 border-blue-600 rounded px-4 py-2 border disabled:text-blue-400 disabled:border-blue-400" :disabled="disabled || loading" :type="type">
+  <button
+    ref="button"
+    class="flex justify-center items-center rounded px-4 py-2 text-white"
+    :class="{
+      'bg-blue-600 hover:bg-blue-700 rounded text-white px-4 py-2': !secondary,
+      'border-blue-600 hover:border-blue-700 disabled:border-blue-400': secondary,
+    }"
+    :disabled="disabled || loading"
+    :type="type"
+  >
     <slot v-if="!loading" />
     <div v-else class="i-heroicons-arrow-path-solid rotate animate-spin animate-count-infinite text-center" />
   </button>
 </template>
+
