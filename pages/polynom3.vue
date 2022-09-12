7bb15432d04363da1d5326ffb01c92318e6783d3
Grzegorz02@ <grzegorz.perun@gmail.com>
2022-09-12 20:05:00 +0200

    feat: add polynom3 visualizer

---
+++ pages/polynom3.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'main',
+})
+</script>
+
+<template>
+  <iframe class="h-[calc(100vh-61px)] w-screen absolute top-[61px]" src="https://www.desmos.com/calculator/jofvghacoj/" />
+</template>
