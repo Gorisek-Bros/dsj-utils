a6dc93e22c74bd4f98d7e877e7319f50498df452
Grzegorz02@ <grzegorz.perun@gmail.com>
2022-09-13 10:45:44 +0200

    chore: comment utils

--- utils/array.ts
+++ utils/array.ts
@@ -1,8 +1,19 @@
+/**
+ *
+ * @param array
+ * @param n
+ * @returns Array of arrays with n elements
+ */
 function* chunk(array: any[], n: number) {
   for (let i = 0; i < array.length; i += n)
     yield array.slice(i, i + n)
 }
 
+/**
+ *
+ * @param array
+ * @returns Array of arrays with same elements
+ */
 function groupBySame(array: any[]) {
   return array.reduce((r, v, i, a) => {
     if (v === a[i - 1])
@@ -15,6 +26,11 @@ function groupBySame(array: any[]) {
   }, [])
 }
 
+/**
+ *
+ * @param array
+ * @returns Array of objects of unique elements
+ */
 function distinct(array: Record<any, any>[]) {
   return [...new Set(array.map(x => JSON.stringify(x)))].map(x => JSON.parse(x))
 }
--- utils/color.ts
+++ utils/color.ts
@@ -1,8 +1,15 @@
+/**
+ *
+ * @param r
+ * @param g
+ * @param b
+ * @returns Hex representation of the color
+ */
 function rgbToHex(r: number, g: number, b: number): string {
   return `0x${[r, g, b].map((x) => {
-        const hex = x.toString(16)
-        return hex.length === 1 ? `0${hex}` : hex
-    }).join('')}`
+    const hex = x.toString(16)
+    return hex.length === 1 ? `0${hex}` : hex
+  }).join('')}`
 }
 
 export { rgbToHex }
